399bb37f719489e3b537c5a70a8640afdf886635
Eduardo Gomez <Eduardo.Gomez@OCTanner.com>
2021-10-09 12:28:35 -0600

    Add Multicolumns example

--- Advanced/list_to_rows.sql
+++ Advanced/list_to_rows.sql
@@ -19,3 +19,37 @@ CONNECT BY
     instr('1,2,3,4,5,6,7,8,9', ',', 1, level - 1) > 0
 ORDER BY item asc    
 ;
+
+-- Multicolumns
+WITH temp AS (
+    SELECT
+        108                name,
+        'test'             project,
+        'Err1, Err2, Err3' error
+    FROM
+        dual
+    UNION ALL
+    SELECT
+        109,
+        'test2',
+        'Err1'
+    FROM
+        dual
+)
+SELECT DISTINCT
+    name,
+    project,
+    TRIM(regexp_substr(str, '[^,]+', 1, level)) str
+FROM
+    (
+        SELECT
+            name,
+            project,
+            error str
+        FROM
+            temp
+    ) t
+CONNECT BY
+    instr(str, ',', 1, level - 1) > 0
+ORDER BY  name
+;
