1ffbad134a88d424e990b54e1ed9bcd24fe21c55
Eduardo Gomez <Eduardo.Gomez@OCTanner.com>
2021-11-02 16:04:41 -0600

    some select examples

--- Basic/select.sql
+++ Basic/select.sql
@@ -1,4 +1,27 @@
-SELECT 
+
+-- Basic Select
+SELECT
     *
+FROM dual
+;
+
+-- Aliases (replacing columnnames with custom name)
+SELECT
+	1 AS my_id
+FROM dual
+;
+
+-- Unions (Consolidating multiple queries into a single result)
+SELECT
+	'A'
+FROM dual UNION ALL
+SELECT
+	'B'
+FROM dual UNION ALL
+SELECT
+	'C'
+FROM dual UNION ALL
+SELECT
+	'D'
 FROM dual
 ;
